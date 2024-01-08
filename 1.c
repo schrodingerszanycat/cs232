0c45d85a9b50175b25341f9f80a12c43135e2e03
schrodingerszanycat <121005817+schrodingerszanycat@users.noreply.github.com>
2024-01-08 04:09:20 +0000

    Make changes

--- 1.c
+++ 1.c
@@ -4,8 +4,8 @@ int main(int argc, char *argv[])
 {
     for (int i = 0; i < argc; i++) 
     {
-        if (argv[i][0] == '-')
-            continue;
+        if (argv[i][0] == '-') continue;
         printf("%s ", argv[i]);
     }
+    return 0;
 }
