285ecf3c726a9555bd71ab3a566d83ccfcdf82af
schrodingerszanycat <121005817+schrodingerszanycat@users.noreply.github.com>
2024-01-08 04:04:44 +0000

    Add

--- 1.c
+++ 1.c
@@ -5,9 +5,7 @@ int main(int argc, char *argv[])
     for (int i = 0; i < argc; i++) 
     {
         if (argv[i][0] == '-')
-        {
-            continue
-        }
-        printf("%s ", argv[i])
+            continue;
+        printf("%s ", argv[i]);
     }
 }
