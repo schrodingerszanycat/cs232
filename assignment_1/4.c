c1d3c4296485c677c432f8e2e2aa5cbc391cbbe3
schrodingerszanycat <121005817+schrodingerszanycat@users.noreply.github.com>
2024-01-13 04:25:26 +0000

    Add

--- assignment_1/4.c
+++ assignment_1/4.c
@@ -2,13 +2,21 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#define BUFSIZ 1024
 
 int main()
 {
     int fd;
-    fd = open("tmp.text", "w+");
+    fd = open("tmp.text", O_RDWR);
     if (fd < 0) 
-        fd = creat("tmp.txt", "w+");
-    int c = getchar();
+        fd = creat("tmp.txt", O_RDWR);
+
+    char buf[BUFSIZ];
+    int n;
+    while ((n = read(0, buf, BUFSIZ)) > 0)
+    {
+        
+    }
+    
     
 }
