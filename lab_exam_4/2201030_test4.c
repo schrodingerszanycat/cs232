99ceb1998365d176fc6be8ec7c1a79914b90361b
schrodingerszanycat <121005817+schrodingerszanycat@users.noreply.github.com>
2024-04-01 07:53:17 +0530

    Update 2201030_test4.c

---
+++ lab_exam_4/2201030_test4.c
@@ -0,0 +1,11 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <semaphore.h>
+#include <stdbool.h>
+#include <sys/time.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
